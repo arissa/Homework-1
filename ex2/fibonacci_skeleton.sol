50d239239426817eecd2f0ca7a5f1d83372f3ce9
arissa@berkeley.edu <arissa@berkeley.edu>
2017-09-14 13:09:59 -0700

    finished Homework 1

--- ex2/fibonacci_skeleton.sol
+++ ex2/fibonacci_skeleton.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.4.16;
 contract Fibonacci {
 	function calculate(uint position) returns (uint result) {
 		/* Add one variable to hold our greeting */
+		if (position < 2) return position;
+		else return Fibonacci.calculate(position-1) + Fibonacci.calculate(position-2);
 	}
 
 	/* Add a fallback function to prevent contract payability and non-existent function calls */
+	function() { }
 }
